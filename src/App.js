2805d22386bee8f88078fa0cefcb109a71cce214
Alexandre <alexandrecarlos2@gmai.com>
2023-01-12 15:21:37 -0300

    busca em api viacep

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  
+  const [endereco, setEndereco] = useState({});
+
+
+const [enderecos, setEnderecos] = useState([]);
+
+function manipularEndereco(evento){
+  const cep = evento.target.value;
+  setEndereco({ 
+      cep
+  });
+
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(response => response.json())
+      .then(dados => {
+        console.log(dados);
+
+        setEnderecos(lista => [...lista, endereco]);
+        console.log(enderecos);
+        setEndereco(enderecoAntigo => 
+          ({
+            ...enderecoAntigo, 
+            rua: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+          }))
+      })
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input placeholder='Digite o Cep' onChange={manipularEndereco} />      
+        <ul>
+          <li>CEP: {endereco.cep}</li>
+          <li>Rua: {endereco.rua}</li>
+          <li>Bairro: {endereco.bairro}</li>
+          <li>Cidade: {endereco.cidade}</li>
+          <li>Estado: {endereco.estado}</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
